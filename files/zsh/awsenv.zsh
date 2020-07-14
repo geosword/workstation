dfbcbc74f921b48c1fa51d37ef40c323f3bb790d
Dylan Humphreys <dylan.humphreys@sectigo.com>
2020-07-14 12:31:37 +0100

    if the aws credentials are setup correctly, assuming multi profile, then just those environment variables are needed for boto and ec2 module to function correctly

--- files/zsh/awsenv.zsh
+++ files/zsh/awsenv.zsh
@@ -1,3 +1,2 @@
 export AWS_REGION="us-east-1"
-export AWS_ACCESS_KEY_ID=$(source <(grep -m1 aws_access_key_id ~/.aws/credentials | tr -d ' '); echo $aws_access_key_id)
-export AWS_SECRET_ACCESS_KEY=$(source <(grep -m1 aws_secret_access_key ~/.aws/credentials | tr -d ' '); echo $aws_secret_access_key)
+export AWS_PROFILE=prod
